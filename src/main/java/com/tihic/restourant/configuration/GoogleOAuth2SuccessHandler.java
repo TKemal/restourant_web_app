5212787b7199f52cb4a1f47ea121559e5423ad40
tihic <tihick@hotmail.com>
2021-05-30 23:41:40 +0200

    Add Auth.

--- src/main/java/com/tihic/restourant/configuration/GoogleOAuth2SuccessHandler.java
+++ src/main/java/com/tihic/restourant/configuration/GoogleOAuth2SuccessHandler.java
@@ -7,7 +7,6 @@ import com.tihic.restourant.model.Role;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
